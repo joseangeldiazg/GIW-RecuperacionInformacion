edb47bb724996348a33675b5e1069092fefbb351
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-05-15 15:54:13 +0200

    Añadida clase usuarios. avanza #4

--- RecommendationSystem/src/recommendationsystem/Usuarios.java
+++ RecommendationSystem/src/recommendationsystem/Usuarios.java
@@ -11,4 +11,43 @@ package recommendationsystem;
  */
 public class Usuarios {
     
+    private int user_id;
+    private String sex;
+    private int age;
+
+    public Usuarios(int user_id, String sex, int age) {
+        this.user_id = user_id;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuarios{" + "user_id=" + user_id + ", sex=" + sex + ", age=" + age + '}';
+    }   
+    
 }
